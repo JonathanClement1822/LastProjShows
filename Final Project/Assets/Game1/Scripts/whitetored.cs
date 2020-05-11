151912342015c5f972f401275999c75f6f68dc89
Jonathan Clement <blowdacon@gmail.com>
2020-05-10 21:21:52 -0400

    Added my game with a comment in the next scene

---
+++ Final Project/Assets/Game1/Scripts/whitetored.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class whitetored : MonoBehaviour
+{
+    public Image image;
+    public float green;
+    public float blue;
+
+    void Update()
+    {
+        if (blue >= 255f)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            blue = blue + 2f;
+            green = green + 2f;
+        }
+        blue = blue - Time.deltaTime;
+        green = green - Time.deltaTime;
+        image = GetComponent<Image>();
+        image.color = new Color(255f, green, blue, 255f);
+/*        while (blue >= -1f && blue < 256f)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                blue = blue + 2;
+                green = green + 2;
+            }
+            else
+            {
+                blue = blue - Time.deltaTime;
+                green = green - Time.deltaTime;
+                image = GetComponent<Image>();
+                image.color = new Color(255f, green, blue, 255f);
+            }
+        }*/
+      
+
+    }
+}
